ac277f9f5a39326727cea9618a9e442409e9cd10
CrispyWaterz <153338815+CrispyWaterz@users.noreply.github.com>
2026-04-30 18:09:48 +0700

    Update twitter_analysis.py

--- twitter_analysis.py
+++ twitter_analysis.py
@@ -308,7 +308,10 @@ hybrid_model.summary()
 To further optimize our model, we'll use Keras Tuner to systematically search for the best combination of hyperparameters, such as the number of filters in `Conv1D` layers, units in `LSTM` layers, and dropout rates. This will help us find a model configuration that potentially yields better performance.
 """
 
-import keras_tuner
+try:
+    import keras_tuner as kt
+except ImportError:
+    kt = None
 
 # Define the model building function for Keras Tuner
 def build_tunable_model(hp):
